42ccba2a6791dc8f7ce4593ecad196a042d3940a
ztgeng <zhtgeng@gmail.com>
2015-10-02 06:18:43 -0700

    Click map amd get address

--- StreetSweeping/app/src/main/java/com/example/geng/streetsweeping/MapsActivity.java
+++ StreetSweeping/app/src/main/java/com/example/geng/streetsweeping/MapsActivity.java
@@ -3,7 +3,9 @@ package com.example.geng.streetsweeping;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,6 +13,10 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,14 +26,20 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.util.Locale;
+
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     LocationManager locationManager;
+
+    TextView streetName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        streetName = (TextView) findViewById(R.id.streetname);
         setUpMapIfNeeded();
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -109,7 +121,18 @@ public class MapsActivity extends FragmentActivity {
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
-                //System.out.println("");
+                Geocoder geocoder = new Geocoder(MapsActivity.this.getApplicationContext(), Locale.getDefault());
+                try {
+                    List<Address> addressList = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+                    //System.out.println(addressList.get(0));
+                    if (addressList != null && !addressList.isEmpty()) {
+                        if (addressList.get(0).getMaxAddressLineIndex() > 0) {
+                            streetName.setText(addressList.get(0).getAddressLine(0));
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
@@ -122,6 +145,20 @@ public class MapsActivity extends FragmentActivity {
             Location lastLocation = locationManager.getLastKnownLocation(bestProvider);
 
             centerMap(lastLocation);
+
+            Geocoder geocoder = new Geocoder(MapsActivity.this.getApplicationContext(), Locale.getDefault());
+            try {
+                List<Address> addressList = geocoder.getFromLocation(lastLocation.getLatitude(), lastLocation.getLongitude(), 1);
+                //System.out.println(addressList.get(0).getAddressLine(0));
+                if (addressList != null && !addressList.isEmpty()) {
+                    if (addressList.get(0).getMaxAddressLineIndex() > 0) {
+                        streetName.setText(addressList.get(0).getAddressLine(0));
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         } else {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION) ||
                     ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
